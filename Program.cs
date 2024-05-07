04278f563aa6e8834d5c205610f2d0031e933db9
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-05-07 20:43:53 +0200

    arrays

--- Program.cs
+++ Program.cs
@@ -7,70 +7,29 @@ namespace Algebra.HelloWorld
     {
         static void Main(string[] args)
         {
-            string rijec = " Algebra  ";
+            string recenica = "Ovo je jedna rečenica";
+            string[] tekst = recenica.Split(' ');
 
-            Console.WriteLine(rijec);
+            if (tekst.Length > 0)
+            {
+                Console.WriteLine(tekst[1]);
+                tekst[0] = "To";
+                Console.WriteLine(tekst.ToString());
+            }
 
-            // operacije/metode nad varijablom:
+            // string[] ljubimci;
+            // string[] ljubimci = new string[2];
+            //string[] ljubimci = new string[] { "pas", "mačka", "ribica" };
+            string[] ljubimci = { "pas", "mačka", "ribica" };
 
-            Console.WriteLine(rijec.ToLower());
-            Console.WriteLine(rijec.ToUpper());
-            Console.WriteLine(rijec.TrimStart(' '));
-            Console.WriteLine(rijec.TrimEnd(' '));
-            Console.WriteLine(rijec.Trim());
+            ljubimci[2] = "hrčak";
 
-            // svojstva varijable:
-            
-            Console.WriteLine(rijec.Length);
-            Console.WriteLine(rijec.TrimStart(' ').Length);
-            Console.WriteLine(rijec.TrimEnd(' ').Length);
-            Console.WriteLine(rijec.Trim().Length);
+            short[] ocjene = { 1, -2, 2, -3, 3, -4, 4, -5, 5 };
 
-            char a = 'a';
-            char l = 'l';
-            char g = 'g';
-            char e = 'e';
-            char b = 'b';
-            char r = 'r';
+            // dvodimenzionalni niz 
 
-            Console.WriteLine("" + a + l + g + e + b + r + a);
-            Console.WriteLine(string.Concat(a, l, g, e, b, r, a));
-
-            // escape characters: \n \\ \" \'
-            Console.WriteLine("Ovo je neki,\n teks \\ t u \"navodnicima\" !?");
-
-            int broj = 1024;
-            Console.WriteLine(broj);
-            Console.WriteLine(broj.ToString());
-
-            Convert.ToString(broj);
-            Int32.Parse(broj.ToString());
-
-            Console.WriteLine(a + l + g + e + b + r + a);
-            var kod = char.GetNumericValue(a);
-
-            // string je niz znakova:
-
-            string jelo = "Lasagna";
-            Console.WriteLine(jelo.First());
-            Console.WriteLine(jelo.Last());
-            Console.WriteLine(jelo.Substring(1));
-            Console.WriteLine(jelo.Substring(2, 3));
-            Console.WriteLine(jelo.IndexOf('n'));
-            Console.WriteLine(jelo.IndexOf("gn"));
-            Console.WriteLine(jelo.IndexOf('ž'));
-
-            string recenica = "Jedna obična rečenica";
-            string[] rijeci = recenica.Split(' ');
-
-            string red = "1234;Pero;Perić;ulica;14"; // CSV
-
-            Console.WriteLine(red.Replace("Pero", "Marko"));
-
-            var polja = red.Split(';');
-            Console.WriteLine(red.Split(';').Last());
-
-            var x = Int32.Parse(red.Split(';').First()).ToString().Split().Length;
+            int[,] matrica = new int[10, 10];
+            matrica[1, 4] = 10;
 
             Console.ReadKey();
         }
