a543b8940907ebc0d4b54343d7d6b6098fdd8cb7
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-05-14 20:58:11 +0200

    init

--- Program.cs
+++ Program.cs
@@ -4,41 +4,114 @@ namespace Algebra.HelloWorld
 {
     internal class Program
     {
+        /// <summary>
+        /// Ovo je početna točka izvršavanja programa.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // vremenska prognoza:
+            // System.Console.BackgroundColor = ConsoleColor.Green;
 
-            // ispisati tekst vremena i mogućnosti za kišu:
+            Console.WriteLine("Ovo je tekst.");
+            Console.Clear();
 
-            Console.Write("Kakvo je vrijeme? ");
-            string vrijeme = Console.ReadLine(); // oblacno, vedro
+            var x = Math.Abs(-4); // funkcija vraća apsolutnu vrijednost
+            var y = Math.Sqrt(x);
 
-            Console.Write("Da li je mogućnost kiše? ");
-            string kisa = Console.ReadLine(); // da/ne
+            Math.Sin(x);
 
-            // priprema
-            bool moguca_kisa = (kisa == "da");
+            var ime = string.Concat("Pero", "Perić");
+            Console.WriteLine(ime.Substring(4));
 
-            // obrada
-            if (vrijeme == "oblacno")
+            long.TryParse("10000", out long broj);
+
+            // IgrajSePisanja();
+            // IgrajSePisanja();
+
+            PrepisiTekst("bilo koji tekst", true);
+            PrepisiTekst("običan tekst");
+
+            string recenica = "Danas je kišno vrijeme";
+            string[] niz = recenica.Split(' ');
+            recenica.Substring(6);
+            recenica.Substring(6, 2);
+
+            Console.WriteLine(Program.ZbrojiDuplo(5));
+            Console.WriteLine(Program.ZbrojiDuplo(45.25));
+
+            Console.Clear();
+
+            IspisiKorisnika("Pero Perić", 32, 42, 20000);
+            IspisiKorisnika("Marta Martić", 25);
+            IspisiKorisnika(ime: "Tonka Tonkić", brojCipela: 39, starost: 36);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Ova metoda ispisuje tekst i ponavlja upisano.
+        /// </summary>
+        static void IgrajSePisanja()
+        {
+            Console.WriteLine("Ovo je prvi red.");
+            Console.WriteLine("Ovo je drugi red.");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Napisali ste: {input}");
+        }
+
+        static void PrepisiTekst(string tekst, bool istaknuto = false)
+        {
+            if (istaknuto)
             {
-                Console.Write("Vrijeme je oblačno. ");
-
-                if (moguca_kisa)
-                {
-                    Console.WriteLine("Ponesite kišobran!");
-                }
-                else
-                {
-                    Console.WriteLine("Ali ne brinite, neće biti kiše. ;)");
-                } 
+                Console.WriteLine(tekst.ToUpper());
+                PrepisiTekst(tekst);
             }
             else
             {
-                Console.WriteLine("Vrijeme će biti prekrasno. :)");
+                Console.WriteLine(tekst);
+            }
+        }
+
+        static void IspisiKorisnika(string ime, int starost, int brojCipela = 0, double prihodi = 0.0)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Podaci o korisniku:\n");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            PrepisiTekst(ime, true);
+
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("\nIme: {0}", ime);
+            Console.WriteLine("Starost: {0}", starost);
+    
+            if (brojCipela > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Broj cipela: {0}", brojCipela);
             }
 
-            Console.ReadKey();
+            if (prihodi > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Prihodi su: {0}", prihodi);
+            }
+
+            Console.WriteLine();
+        }
+
+        static int ZbrojiDuplo(int broj)
+        {
+            broj += broj;
+            return broj;
+        }
+
+        static double ZbrojiDuplo(double broj)
+        {
+            broj += broj;
+            return broj;
         }
     }
 }
