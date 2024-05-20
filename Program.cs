989e5bf2d79372c06bccf80d5bc22efd58e0338e
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-05-20 20:40:01 +0200

    breeze

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Algebra.HelloWorld
 {
@@ -10,108 +11,97 @@ namespace Algebra.HelloWorld
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // System.Console.BackgroundColor = ConsoleColor.Green;
+            Jabuka jabuka1 = new Jabuka();
+            jabuka1.IspisiSvojstva();
 
-            Console.WriteLine("Ovo je tekst.");
-            Console.Clear();
+            //jabuka1.Boja = "žuta";
+            jabuka1.TezinaG += 20;
 
-            var x = Math.Abs(-4); // funkcija vraća apsolutnu vrijednost
-            var y = Math.Sqrt(x);
+            jabuka1.IspisiSvojstva();
 
-            Math.Sin(x);
+            jabuka1 = null;
 
-            var ime = string.Concat("Pero", "Perić");
-            Console.WriteLine(ime.Substring(4));
+            Jabuka jabuka2 = new Jabuka("crvena", 75);
+            jabuka2.Kupi();
 
-            long.TryParse("10000", out long broj);
-
-            // IgrajSePisanja();
-            // IgrajSePisanja();
-
-            PrepisiTekst("bilo koji tekst", true);
-            PrepisiTekst("običan tekst");
-
-            string recenica = "Danas je kišno vrijeme";
-            string[] niz = recenica.Split(' ');
-            recenica.Substring(6);
-            recenica.Substring(6, 2);
-
-            Console.WriteLine(Program.ZbrojiDuplo(5));
-            Console.WriteLine(Program.ZbrojiDuplo(45.25));
-
-            Console.Clear();
-
-            IspisiKorisnika("Pero Perić", 32, 42, 20000);
-            IspisiKorisnika("Marta Martić", 25);
-            IspisiKorisnika(ime: "Tonka Tonkić", brojCipela: 39, starost: 36);
+            Jabuka jabuka3 = new Jabuka();
+            jabuka3.BioUzgoj = true;
+            jabuka3.Kupi();
 
             Console.ReadKey();
         }
-
-        /// <summary>
-        /// Ova metoda ispisuje tekst i ponavlja upisano.
-        /// </summary>
-        static void IgrajSePisanja()
-        {
-            Console.WriteLine("Ovo je prvi red.");
-            Console.WriteLine("Ovo je drugi red.");
-            var input = Console.ReadLine();
-            Console.WriteLine($"Napisali ste: {input}");
-        }
-
-        static void PrepisiTekst(string tekst, bool istaknuto = false)
-        {
-            if (istaknuto)
-            {
-                Console.WriteLine(tekst.ToUpper());
-                PrepisiTekst(tekst);
-            }
-            else
-            {
-                Console.WriteLine(tekst);
-            }
-        }
-
-        static void IspisiKorisnika(string ime, int starost, int brojCipela = 0, double prihodi = 0.0)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-
-            Console.WriteLine("Podaci o korisniku:\n");
-
-            Console.BackgroundColor = ConsoleColor.Yellow;
-
-            PrepisiTekst(ime, true);
-
-            Console.BackgroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("\nIme: {0}", ime);
-            Console.WriteLine("Starost: {0}", starost);
-    
-            if (brojCipela > 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Broj cipela: {0}", brojCipela);
-            }
-
-            if (prihodi > 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Prihodi su: {0}", prihodi);
-            }
-
-            Console.WriteLine();
-        }
-
-        static int ZbrojiDuplo(int broj)
-        {
-            broj += broj;
-            return broj;
-        }
-
-        static double ZbrojiDuplo(double broj)
-        {
-            broj += broj;
-            return broj;
-        }
     }
+
+    class Jabuka
+    {
+        // privatne varijable
+        private string _boja;
+        private double _tezinaG;
+        private bool _bioUzgoj;
+
+        #region svojstva
+
+        //public string Boja;
+
+        public string Boja
+        { 
+            get { return _boja; }
+        }
+
+        //public double TezinaG;
+
+        public double TezinaG
+        {
+            get { return _tezinaG; }
+            set { _tezinaG = value; }
+        }
+
+        //public bool BioUzgoj;
+        public bool BioUzgoj
+        {
+            set { _bioUzgoj = value; }
+        }
+
+        public string Oblik { get; set; }
+
+        #endregion
+
+        // konstruktori
+        public Jabuka() 
+        {
+            Console.WriteLine("Konstrukcija objekta.");
+
+            _boja = "zelena";
+            TezinaG = 80;
+        }
+
+        public Jabuka(string boja, double tezinaG)
+        {
+            _boja = boja;
+            TezinaG = tezinaG;
+        }
+
+        #region metode
+
+        public void Kupi()
+        {
+            Console.WriteLine("Metoda Kupi je pozvana.");
+        }
+
+        public void Uberi()
+        {
+        }
+
+        public void IspisiSvojstva()
+        {
+            Console.WriteLine($"Boja: {_boja}");
+            Console.WriteLine($"BioUzgoj: {_bioUzgoj}");
+            Console.WriteLine($"Težina (u gramima): {_tezinaG}");
+        }
+
+        #endregion
+
+        // privatne metode
+    }
+
 }
