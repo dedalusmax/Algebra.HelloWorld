42d1a6ab4bffa4e6f6584112a79614d11dd031e1
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-05-21 20:12:07 +0200

    models

--- Models/Osoba.cs
+++ Models/Osoba.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace HelloWorld.Models
 {
     public class Osoba
     {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HelloWorld.Models;
 
 namespace HelloWorld
 {
