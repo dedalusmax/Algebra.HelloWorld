1014a066ba5867a32a076f27443bfae2cc9b9067
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-06-27 21:05:51 +0200

    aa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Algebra.HelloWorld
 {
@@ -6,36 +8,60 @@ namespace Algebra.HelloWorld
     {
         static void Main(string[] args)
         {
-            int strikni = 10;
-            //Nullable<int> slobodni = null;
-            int? slobodni = null;
-
-            strikni = slobodni ?? 25;
-
-            slobodni += 2;
-
-            //strikni += slobodni.Value;
-
-            strikni += slobodni.GetValueOrDefault();
-
-            if (!slobodni.HasValue)
+            List<string> lista = new List<string>
             {
-                Console.WriteLine("varijabla je null");
-            }
+                "Pero",
+                "Iva",
+                "Marko"
+            };
 
-            Console.WriteLine(strikni);
-            Console.WriteLine(slobodni);
+            var rezultat = lista.FindAll(x => x.Equals("Pero"));
+
+            string imena = string.Empty;
+            //lista.ForEach(x => { imena += x; });
+            //lista.ForEach((x) => imena += x);
+            lista.ForEach(x => imena += x);
+
+            List<Osoba> osobe = new List<Osoba>
+            {
+                new Osoba("Pero"),
+                new Osoba("Iva"),
+                new Osoba("Marko"),
+            };
+
+            var imena2 = osobe.Select(x => x.Ime).ToList();
+
+            Action akcija = () => Console.WriteLine("Akcija izvršena!");
+            akcija.Invoke();
+
+            // Func<string, bool> funkcija = (x) => true;
+            Func<string, int, Osoba> kreirajOsobu = (ime, oib) => { return new Osoba(ime, oib); };
+
+            kreirajOsobu("Pero", 123);
+
+            Func<string, int, Osoba> delegat = delegate (string s, int x) { 
+                return new Osoba(s); 
+            };
 
             Console.ReadKey();
         }
 
-        private class Osoba
+        public class Osoba 
         {
-            public int Sifra { get; set; }
+            public int? OIB { get; set; }
 
             public string Ime { get; set; }
 
-            public Nullable<int> OIB { get; set; }
+            public Osoba(string ime)
+            {
+                Ime = ime;
+            }
+
+            public Osoba(string ime, int oib)
+            {
+                Ime = ime;
+                OIB = oib;
+            }
         }
     }
 }
