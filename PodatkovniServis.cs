61c800ab6016e0cae7d748d1b67c09d601a46317
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-07-23 20:52:45 +0200

    CRUD operations

---
+++ PodatkovniServis.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+
+namespace HelloWorld
+{
+    internal class PodatkovniServis
+    {
+        private readonly LinqToSqlDataContext _dataContext;
+
+        public PodatkovniServis(LinqToSqlDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void UnosNovogZaposlenika(Zaposlenik zaposlenik)
+        {
+            _dataContext.Zaposleniks.InsertOnSubmit(zaposlenik);
+            _dataContext.SubmitChanges();
+        }
+
+        public void UnosNovogZaposlenika(string imePrezime, string email, string telefon, string adresa, int odjelId)
+        {
+            var zaposlenik = new Zaposlenik
+            {
+                ImePrezime = imePrezime,
+                Email = email,
+                Telefon = telefon,
+                Adresa = adresa,
+                OdjelID = odjelId
+            };
+
+            _dataContext.Zaposleniks.InsertOnSubmit(zaposlenik);
+            _dataContext.SubmitChanges();
+        }
+
+        public void AzuriranjeZaposlenika(Zaposlenik zaposlenik)
+        {
+            _dataContext.SubmitChanges();
+        }
+
+        public void AzuriranjeZaposlenika(int id, string telefon, string adresa)
+        {
+            var zaposlenik = DohvatiZaposlenika(id);
+            if (zaposlenik != null)
+            {
+                zaposlenik.Telefon = telefon;
+                zaposlenik.Adresa = adresa;
+
+                _dataContext.SubmitChanges();
+            }
+        }
+
+        public Zaposlenik DohvatiZaposlenika(int id)
+        {
+            return _dataContext.Zaposleniks.SingleOrDefault(_ => _.ID == id);
+        }
+
+        public Zaposlenik DohvatiZaposlenika(string imePrezime)
+        {
+            return _dataContext.Zaposleniks.Where(_ => _.ImePrezime.Contains(imePrezime))
+                .FirstOrDefault();
+        }
+
+        public void ObrisiZaposlenika(int id)
+        {
+            var zaposlenik = DohvatiZaposlenika(id);
+
+            _dataContext.Zaposleniks.DeleteOnSubmit(zaposlenik);
+            _dataContext.SubmitChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HelloWorld;
+using System;
+using System.Configuration;
+using System.Linq;
 
 namespace Algebra.HelloWorld
 {
@@ -6,9 +9,61 @@ namespace Algebra.HelloWorld
     {
         static void Main(string[] args)
         {
+            var connStr = ConfigurationManager.ConnectionStrings["DbConnStr"].ConnectionString;
+            var dbContext = new LinqToSqlDataContext(connStr);
 
+            // var dbContext = new LinqToSqlDataContext("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=linq_to_sql;Integrated Security=True");
+
+            // DML 
+
+            // UnosZaposlenika(dbContext);
+
+            var servis = new PodatkovniServis(dbContext);
+
+            //servis.UnosNovogZaposlenika(new Zaposlenik
+            //{
+            //    ImePrezime = "Iva Ivić",
+            //    Email = "iva.ivic@algebra.hr",
+            //    Telefon = "095444333222",
+            //    Adresa = "Livadska 12",
+            //    OdjelID = 2
+            //});
+
+            // DQL 
+
+            var upit = dbContext.Zaposleniks.FirstOrDefault();
+
+            //var upit = from zaposlenik in dbContext.Zaposleniks
+            //           where zaposlenik.ImePrezime.StartsWith("Pero")
+            //           select zaposlenik;
+
+            var zaposlenik = servis.DohvatiZaposlenika("Pero");
+            zaposlenik.Telefon = "+385 91 333 444 555";
+            zaposlenik.OdjelID = 3;
+
+            servis.AzuriranjeZaposlenika(zaposlenik);
+
+            servis.AzuriranjeZaposlenika(2, "+385 42 111 222", "Kolodvorska 145");
+
+            servis.ObrisiZaposlenika(2);
 
             Console.ReadKey();
+        }
+
+        private static void UnosZaposlenika(LinqToSqlDataContext dbContext)
+        {
+            var zaposlenik = new Zaposlenik
+            {
+                ImePrezime = "Pero Perić",
+                Email = "pero.peric@algebra.hr",
+                Telefon = "09811122233",
+                Adresa = "Vrtna bb",
+                OdjelID = 1
+            };
+
+            dbContext.Zaposleniks.InsertOnSubmit(zaposlenik);
+
+            dbContext.SubmitChanges();
         }
     }
 }
